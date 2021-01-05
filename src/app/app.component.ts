29c52f559aa29b14c603bda4840dfdeadbfb2f3f
kousiclattala <kousiclattala112@gmail.com>
2021-01-05 15:57:58 +0530

    First Angular App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'counter-app';
+  title: string = 'Counter App';
+
+  count: number = 0;
+
+  handleIncrease = () => {
+    this.count = this.count + 1;
+
+    if (this.count === 11) {
+      alert(`You have reached the counter limit ${this.count}`);
+      this.count = 0;
+    }
+  }
+
+  handleDecrease = () => {
+
+    if (this.count > 0) {
+      this.count = this.count - 1;
+    }else{
+      alert(`You reached the end of counter limit ${this.count}`) 
+
+    }
+
+  }
+
+  handleReset = () => {
+    this.count = 0;
+  }
 }
